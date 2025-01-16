705f8002d4899685860472470fef95b54cf935a3
austincampb <acampb@unc.edu>
2025-01-15 21:11:20 -0500

    change font

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ const App = () => {
   const categories = [
     {
       id: 1,
-      title: 'hats',
+      title: 'Hats',
       img: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
-      title: 'jackets',
+      title: 'Jackets',
       img: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
-      title: 'sneakers',
+      title: 'Sneakers',
       img: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
-      title: 'womens',
+      title: 'Womens',
       img: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
-      title: 'mens',
+      title: 'Mens',
       img: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
